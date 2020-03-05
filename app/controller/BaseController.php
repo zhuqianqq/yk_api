67fdae7527d9f99f48f75ee826353d1eaf2f9e2d
zhoufumin <411673347@qq.com>
2020-03-05 22:08:46 +0800

    1

--- app/controller/BaseController.php
+++ app/controller/BaseController.php
@@ -82,10 +82,10 @@ abstract class BaseController
     protected function initialize()
     {
         if ($this->cors) {
-            header("Access-Control-Allow-Origin:*");
+            //header("Access-Control-Allow-Origin:*");
             header("Access-Control-Allow-Credentials: true");
             header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-            header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With,appVersion,channelId,gid,osVersion");
+            header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With,user-id,access-key");
             header("Content-Type: application/json; charset=utf-8");
         }
 
