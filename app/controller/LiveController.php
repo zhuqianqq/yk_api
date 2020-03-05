204f33b18ee6a1904964e9b07ced4cb8469c4fff
liyiwen <liyw@inke.cn>
2020-03-05 20:20:22 +0800

    no message

--- app/controller/LiveController.php
+++ app/controller/LiveController.php
@@ -17,9 +17,21 @@ class LiveController extends BaseController
     ];
 
     /**
-     * 用户商品列表
+     * 直播列表
      */
-    public function getLiveInfo()
+    public function getList()
+    {
+        $page_size = $this->request->param("page_size", 10, "intval");
+
+        $list = Db::name('t_room')->order('id', 'desc')->paginate($page_size);
+
+        return $this->outJson(0, "success", $list);
+    }
+
+    /**
+     * 直播详情
+     */
+    public function getInfo()
     {
         $room_id = $this->request->param("room_id", 1, "intval");
 
@@ -28,6 +40,22 @@ class LiveController extends BaseController
         }
 
         $data = TRoom::where('room_id', $room_id)->find();
+
+        return $this->outJson(0, "success", $data);
+    }
+
+    /**
+     * 根据user_id查询直播详情
+     */
+    public function getInfoByUid()
+    {
+        $user_id = $this->request->param("user_id", 1, "intval");
+
+        if ($user_id <= 0) {
+            return $this->outJson(100, "user_id无效");
+        }
+
+        $data = TRoom::where('user_id', $user_id)->find();
 
         return $this->outJson(0, "success", $data);
     }
