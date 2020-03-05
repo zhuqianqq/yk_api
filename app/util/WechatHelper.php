183e09dd3ef2c7e032a12f2e8b644a2688f754fd
gaoshangguo <gaoshangguo@gmail.com>
2020-03-05 17:30:10 +0800

    update

--- app/util/WechatHelper.php
+++ app/util/WechatHelper.php
@@ -3,6 +3,7 @@
  */
 namespace app\util;
 
+use think\facade\Config;
 use think\facade\Cache;
 use think\facade\Db;
 
@@ -13,11 +14,11 @@ class WechatHelper
     {
         $wechat_config = Config::get('weixin');
         $realUrl = 'https://api.weixin.qq.com/sns/jscode2session?appid=' . $wechat_config['appid'] . '&secret=' . $wechat_config['secret'] . '&js_code=' . $code . '&grant_type=authorization_code';
-        $res = json_decode(Tools::curlGet($realUrl));
-        if ($res->openid == null) {
+        $res = Tools::curlGet($realUrl, null);
+        if ($res == null || !isset($res["openid"])) {
             return "";
         }
-        return $res->openid;
+        return $res["openid"];
     }
 
 }
