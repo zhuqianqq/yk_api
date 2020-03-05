af99968358c9783451ab60a5b54d9f5ae7ded476
liyiwen <liyw@inke.cn>
2020-03-05 20:08:39 +0800

    no message

---
+++ app/controller/LiveController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * 直播
+ */
+
+namespace app\controller;
+
+use App\Models\Cfund\Project;
+use app\model\TRoom;
+use app\util\Tools;
+use think\facade\Db;
+
+class LiveController extends BaseController
+{
+    protected $middleware = [
+        'access_check' => ['only' => ['']],
+    ];
+
+    /**
+     * 用户商品列表
+     */
+    public function getLiveInfo()
+    {
+        $room_id = $this->request->param("room_id", 1, "intval");
+
+        if ($room_id <= 0) {
+            return $this->outJson(100, "room_id无效");
+        }
+
+        $data = TRoom::where('room_id', $room_id)->find();
+
+        return $this->outJson(0, "success", $data);
+    }
+}
---
+++ app/model/TRoom.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\model;
+
+use think\facade\Db;
+
+class TRoom extends BaseModel
+{
+    protected $table = "t_room";
+}
