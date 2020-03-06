1f877e0910a84412276f3b9d0fe974d01bdf222e
gaoshangguo <gaoshangguo@gmail.com>
2020-03-06 12:04:07 +0800

    文件由http改为https

--- app/util/CosHelper.php
+++ app/util/CosHelper.php
@@ -40,7 +40,7 @@ class CosHelper
 
             if ($result && !empty($result["Location"])) {
                 return Tools::outJson(0, "上传成功", [
-                    "url" => strpos($result["Location"], "http") == 0 ? $result["Location"] : "http://" . $result["Location"],
+                    "url" => strpos($result["Location"], "https") == 0 ? $result["Location"] : "https://" . $result["Location"],
                     "key" => $key,
                 ]);
             }
