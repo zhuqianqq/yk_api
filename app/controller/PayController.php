ec954ae8d5e6fccf0abdd3471a0c774bb0045678
zhoufumin <411673347@qq.com>
2020-03-06 20:47:55 +0800

    1

--- app/controller/PayController.php
+++ app/controller/PayController.php
@@ -19,7 +19,7 @@ class PayController extends BaseController
      */
     public function alipayNotify()
     {
-        Tools::addLog("aliapy_notify", "支付宝回调开始,praram:" . $this->request->getInput());
+        Tools::addLog("alipay_notify", "支付宝回调开始,praram:" . $this->request->getInput());
         $aliPay = new AlipayService();
         //首先必需验证签名，然后验证是否是支付宝发来的通知。
         $verify_result = $aliPay->verifyNotify();
@@ -28,14 +28,14 @@ class PayController extends BaseController
             $result = $aliPay->alipayNotify($this->request->param());
 
             if ($result) {
-                Tools::addLog("aliapy_notify", "支付宝回调通知处理成功");
+                Tools::addLog("alipay_notify", "支付宝回调通知处理成功");
                 exit("success"); //成功时返回success
             } else {
-                Tools::addLog("aliapy_notify", "支付宝回调通知处理失败");
+                Tools::addLog("alipay_notify", "支付宝回调通知处理失败");
                 exit("failed");
             }
         } else {
-            Tools::addLog("aliapy_notify", "支付宝回调验证签名失败");
+            Tools::addLog("alipay_notify", "支付宝回调验证签名失败");
             exit("failed");
         }
     }
