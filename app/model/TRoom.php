56495eff4fa74562c6a6db7f994d9297e9227b16
gaoshangguo <gaoshangguo@gmail.com>
2020-03-06 10:14:48 +0800

    更新房间列表。

--- app/model/TRoom.php
+++ app/model/TRoom.php
@@ -6,4 +6,24 @@ use think\facade\Db;
 class TRoom extends BaseModel
 {
     protected $table = "t_room";
+
+    public static function getList($page,$page_size){
+        $page = $page ? $page : 1;
+        $query = Db::table("t_room r")
+            ->leftJoin("t_member m", "r.user_id=m.user_id ")
+            ->field("r.*,m.nick_name,m.avatar,m.display_code");
+        $list=$query->page($page, $page_size)->select();
+        $total = $query->count();
+        $has_next=0;
+        self::checkHasNextPage($list,$page_size,$has_next);
+        $data = [
+            "list" => $list,
+            "current_page" => $page,
+            "total" => $total,
+            "has_next" => $has_next, //是否有下一页
+        ];
+        return $data;
+    }
+
+
 }
