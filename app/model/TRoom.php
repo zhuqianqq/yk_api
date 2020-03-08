fd4c073ff5555d156399e3a18fdde6dcf241b726
zhoufumin <411673347@qq.com>
2020-03-08 18:58:39 +0800

    1

--- app/model/TRoom.php
+++ app/model/TRoom.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\model;
 
 use app\util\Tools;
@@ -8,21 +9,26 @@ class TRoom extends BaseModel
 {
     protected $table = "t_room";
 
-    public static function getList($page,$page_size){
+    public static function getList($page, $page_size)
+    {
         $page = $page ? $page : 1;
         $query = Db::table("t_room r")
             ->leftJoin("t_member m", "r.user_id=m.user_id ")
             ->field("r.*,m.nick_name,m.avatar,m.display_code");
-        $list=$query->order("like_count",'desc')->page($page, $page_size)->select();
+
+        $list = $query->order("like_count", 'desc')->page($page, $page_size)->select();
         $total = $query->count();
-        $has_next=0;
-        self::checkHasNextPage($list,$page_size,$has_next);
+        $has_next = 0;
+
+        self::checkHasNextPage($list, $page_size, $has_next);
+
         $data = [
             "list" => $list,
             "current_page" => $page,
             "total" => $total,
             "has_next" => $has_next, //是否有下一页
         ];
+
         return $data;
     }
 
@@ -33,13 +39,13 @@ class TRoom extends BaseModel
      * @param $user_id
      * @param $oper_user 操作用户
      */
-    public static function closeRoom($room_id,$user_id,$oper_user = '')
+    public static function closeRoom($room_id, $user_id, $oper_user = '')
     {
         $data = Db::table("t_room")->where(["room_id" => $room_id])->find();
         if (empty($data)) {
             return Tools::outJson(100, "room_id未开播");
         }
-        if($data["user_id"] != $user_id){
+        if ($data["user_id"] != $user_id) {
             return Tools::outJson(100, "你无权关闭该直播");
         }
         unset($data["id"]);
@@ -47,7 +53,7 @@ class TRoom extends BaseModel
         $data["close_time"] = date("Y-m-d H:i:s"); //关播时间
 
         Db::startTrans();
-        Db::table("t_room")->where(["room_id" => $room_id,"user_id" => $user_id])->delete();
+        Db::table("t_room")->where(["room_id" => $room_id, "user_id" => $user_id])->delete();
         Db::table("t_room_history")->insert($data);
         Db::commit();
 
