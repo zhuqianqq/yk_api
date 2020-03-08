32beeab8d5647e70e991f917ad2f5ab97e2f8edd
zhoufumin <411673347@qq.com>
2020-03-08 13:00:18 +0800

    优化

--- app/command/LiveCheckCommand.php
+++ app/command/LiveCheckCommand.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 检测直播是否在线
+ * 检测直播是否在线，不在线的自动下播
  */
 namespace app\command;
 
@@ -30,30 +30,32 @@ class LiveCheckCommand extends BaseCommand
             }
 
             $online_room_ids = $this->getOnlineRooms();
+            if($online_room_ids === false){
+                sleep(2);
+                continue;
+            }
+            $this->log("online_room_ids:".json_encode($online_room_ids));
 
-            $this->log("tencent在线直播:".json_encode($online_room_ids));
-
-            $not_online_list = TRoom::where("room_id","not in",$online_room_ids)->field("room_id,user_id")->select();
-
-            $this->log("共".count($not_online_list)."个不在线直播");
+            $not_online_list = TRoom::where("room_id","not in",$online_room_ids)->field("room_id,user_id")->select(); //不在线的直播
+            $this->log("not_online_list count:".count($not_online_list));
 
             if(!empty($not_online_list)){
                 $not_online_list = $not_online_list->toArray();
-                foreach($not_online_list as $item){
-                    $ret = TRoom::closeRoom($item["room_id"],$item["user_id"],"system");
-                    $this->log("关闭直播:room_id:".$item["room_id"].",ret:".json_encode($ret,JSON_UNESCAPED_UNICODE));
+                foreach($not_online_list as &$item){
+                    $ret = TRoom::closeRoom($item["room_id"],$item["user_id"],"system"); //系统自动下播
+                    $this->log("close room_id:{$item['room_id']},user_id:{$item['user_id']},ret:".json_encode($ret,JSON_UNESCAPED_UNICODE));
                 }
-
-                sleep(15);
-            }else{
-                sleep(15);
+                unset($item);
             }
+
+            sleep(10);
         }
         $this->log("done");
     }
 
     /**
-     * @return array
+     * 获取在线直播列表
+     * @return array|bool 失败时返回false
      */
     public function getOnlineRooms()
     {
@@ -61,27 +63,44 @@ class LiveCheckCommand extends BaseCommand
         $conf = Config::get("tencent_clound");
         $cred = new Credential($conf["secretId"],$conf["secretKey"]);
 
-        // # 实例化要请求产品(以cvm为例)的client对象
+        //实例化要请求client对象
         $client = new LiveClient($cred, "");
 
-        // 实例化一个请求对象
-        $req = new DescribeLiveStreamOnlineListRequest();
-        //$req->AppName = "live";
-        $req->PageNum = 1;
-        $req->PageSize = 100;
+        $online_room_ids = []; //所有在线room_ids
+        $req = new DescribeLiveStreamOnlineListRequest(); // 实例化一个请求对象
+        $page_num = 1;
+        $page_size = 100;
+        try{
+            do{
+                //$req->AppName = "live";
+                $req->PageNum = $page_num; //取得第几页，默认1。
+                $req->PageSize = $page_size;//每页大小，最大100。取值：10~100之间的任意整数。默认值：10。
 
-        $res = $client->DescribeLiveStreamOnlineList($req);
+                $res = $client->DescribeLiveStreamOnlineList($req);
+                $arr = $res->serialize();
+                if(empty($arr)){
+                    usleep(200000);
+                    continue;
+                }
+                //print_r($arr);
+                $this->log("pagenum:{$page_num},page_size:{$page_size},res:\n".json_encode($arr,JSON_UNESCAPED_UNICODE));
 
-        $arr = $res->serialize();
-        $OnlineInfo = $arr['OnlineInfo'] ?? [];
+                $OnlineInfo = $arr['OnlineInfo'] ?? []; //正在推送流的信息列表
+                if(empty($OnlineInfo)){
+                    break;
+                }
+                foreach($OnlineInfo as $item){
+                    $arr = explode("_",$item['StreamName']); //1400319314_101062
+                    $room_id = "room_".$arr[1];
+                    $online_room_ids[] = $room_id;
+                }
+                $page_num ++;
+            }while(true);
 
-        $online_room_ids = [];
-        foreach($OnlineInfo as $item){
-            $arr = explode("_",$item['StreamName']); //1400319314_101062
-            $room_id = "room_".$arr[1];
-            $online_room_ids[] = $room_id;
+            return $online_room_ids;
+        }catch (\Exception $ex){
+            $this->log("getOnlineRooms error:".$ex->getMessage());
+            return false;
         }
-
-        return $online_room_ids;
     }
 }
